43dc76317536c18d13d23aa37c84a07a7f01bd52
daniarfadd <55595228+daniarfadd@users.noreply.github.com>
2021-01-13 22:15:18 +0700

    Replace container and frame div with Paper component

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from "react";
 import "./styles.css";
 
+import Paper from "./components/Paper";
+
 export default function App() {
   return (
-    <div class="container">
-      <div class="frame">
-        <section class="header">
-          <button class="header-btn main-black-color">Add</button>
-          <h1 class="header-title">Todo List</h1>
-          <button class="header-btn main-red-color">Clear</button>
-        </section>
+    <Paper>
+      <section class="header">
+        <button class="header-btn main-black-color">Add</button>
+        <h1 class="header-title">Todo List</h1>
+        <button class="header-btn main-red-color">Clear</button>
+      </section>
 
-        <section class="add">
-          <form action="" class="add-form">
-            <input type="text" class="add-input" />
-            <button class="add-btn main-black-color">Add</button>
-          </form>
-        </section>
+      <section class="add">
+        <form action="" class="add-form">
+          <input type="text" class="add-input" />
+          <button class="add-btn main-black-color">Add</button>
+        </form>
+      </section>
 
-        <section class="todos">
-          <div class="todo">
-            <span class="todo-text">Learning React</span>
-          </div>
-        </section>
-      </div>
-    </div>
+      <section class="todos">
+        <div class="todo">
+          <span class="todo-text">Learning React</span>
+        </div>
+      </section>
+    </Paper>
   );
 }
---
+++ src/components/Paper.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+// mengimport react dari library react
+import PropTypes from "prop-types";
+
+const Paper = ({ children }) => {
+  return (
+    <div className="container">
+      <div className="frame">{children}</div>
+    </div>
+  );
+};
+// ini merupakan function yang akan me-return JSX
+
+Paper.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+    // disini artinya 'children' akan memiliki 1 diantara 2 type yang kita definisikan
+    // bisa berupa satu react node
+    // atau bisa juga berupa sekumpulan (arrayof) react node
+  ])
+};
+// Ini berfungsi untuk mendefinisikan type dari props 'children'
+
+export default Paper;
+// Agar bisa digunakkan di file yang lain
